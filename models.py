ded6ccbab212147a622e64a69d454785512572d9
antoinedang <antoine.dangeard@gmail.com>
2023-02-05 22:54:51 -0500

    added momentum, regularization, non-linear bases
    all that needs to be done is add batches to gradient descent

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+#closed form linear regression
 class LinearRegression:
     def __init__(self, add_bias=True):
         self.add_bias = add_bias
@@ -20,75 +21,58 @@ class LinearRegression:
         yh = x@self.w                             #predict the y values
         return yh
 
+#closed form with L2 regularization
+class L2RegularizedLinearRegression:
+    def __init__(self, add_bias=True, l2_reg=0):
+        self.add_bias = add_bias
+        #l2 reg decides strength (coefficient) of regularization
+        self.l2_reg = l2_reg
+        pass
+            
+    def fit(self, x, y):
+        if x.ndim == 1:
+            x = x[:, None]                         #add a dimension for the features
+        N = x.shape[0]
+        if self.add_bias:
+            x = np.column_stack([x,np.ones(N)])    #add bias by adding a constant feature of value 1
+        #identity matrix might need to be size D
+        self.w = np.linalg.inv(x.T @ x + self.l2_reg*np.identity(N))@x.T@y
+        return self
+    
+    def predict(self, x):                       #add a dimension for the features
+        N = x.shape[0]
+        if self.add_bias:
+            x = np.column_stack([x,np.ones(N)])
+        yh = x@self.w                             #predict the y values
+        return yh
+
 class GradientDescent:
-    def __init__(self, learning_rate=.001, max_iters=1e4, epsilon=1e-8, record_history=False):
+    def __init__(self, learning_rate=.001, max_iters=1e4, epsilon=1e-8, momentum=0):
         self.learning_rate = learning_rate
         self.max_iters = max_iters
-        self.record_history = record_history
         self.epsilon = epsilon
-        if record_history:
-            self.w_history = []                 #to store the weight history for visualization
+        self.momentum = momentum
+        self.previousGrad = None
             
     def run(self, gradient_fn, x, y, w):
         grad = np.inf
         t = 1
         while np.linalg.norm(grad) > self.epsilon and t < self.max_iters:
             grad = gradient_fn(x, y, w)               # compute the gradient with present weight
-            self.w_history.append(w)
+            if previousGrad == None: previousGrad = grad
+            grad = grad*(1.0-self.momentum) + previousGrad*self.momentum
+            previousGrad = grad
             w = w - self.learning_rate * grad         # weight update step
-            if self.record_history:
-                self.w_history.append(w)
             t += 1
         return w
 
-class LogisticRegression:
-    def __init__(self, add_bias=True, learning_rate=.1, epsilon=1e-4, max_iters=1e5, verbose=False):
+#gradient descent regression with options for any combinations of non-linear bases, l1, l2 regularization
+class RegressionWithBasesAndRegularization:
+    def __init__(self, add_bias=True, non_linear_base_fn = (lambda x: x), l1_lambda=0, l2_lambda=0):
         self.add_bias = add_bias
-        self.learning_rate = learning_rate
-        self.epsilon = epsilon                        #to get the tolerance for the norm of gradients 
-        self.max_iters = max_iters                    #maximum number of iteration of gradient descent
-        self.verbose = verbose
-        
-    def fit(self, x, y):
-        if x.ndim == 1:
-            x = x[:, None]
-        if self.add_bias:
-            N = x.shape[0]
-            x = np.column_stack([x,np.ones(N)])
-        N,D = x.shape
-        self.w = np.zeros(D)
-        g = np.inf 
-        t = 0
-        # the code snippet below is for gradient descent
-        while np.linalg.norm(g) > self.epsilon and t < self.max_iters:
-            g = self.gradient(x, y)
-            self.w = self.w - self.learning_rate * g 
-            t += 1
-        
-        if self.verbose:
-            print(f'terminated after {t} iterations, with norm of the gradient equal to {np.linalg.norm(g)}')
-            print(f'the weight found: {self.w}')
-        return self
-    
-    def predict(self, x):
-        if x.ndim == 1:
-            x = x[:, None]
-        Nt = x.shape[0]
-        if self.add_bias:
-            x = np.column_stack([x,np.ones(Nt)])
-        yh = logistic(np.dot(x,self.w))            #predict output
-        return yh
-
-    def gradient(self, x, y):
-        N,D = x.shape
-        yh = logistic(np.dot(x, self.w))    # predictions  size N
-        grad = np.dot(x.T, yh - y)/N        # divide by N because cost is mean over N points
-        return grad                         # size D
-
-class LinearRegressionGradientDescent:
-    def __init__(self, add_bias=True):
-        self.add_bias = add_bias
-        pass
+        self.non_linear_base_fn = non_linear_base_fn
+        self.l1_lambda = l1_lambda
+        self.l2_lambda = l2_lambda
             
     def fit(self, x, y, optimizer):
         if x.ndim == 1:
@@ -98,9 +82,15 @@ class LinearRegressionGradientDescent:
             x = np.column_stack([x,np.ones(N)])
         N,D = x.shape
         def gradient(x, y, w):                          # define the gradient function
-            yh =  x @ w 
+            yh =  self.non_linear_base_fn(x @ w) 
             N, D = x.shape
             grad = .5*np.dot(yh - y, x)/N
+            if self.add_bias:
+                grad[1:] += self.l1_lambda * np.sign(w[1:])
+                grad[1:] += self.l2_lambda * w[1:]
+            else:
+                grad += self.l1_lambda * np.sign(w)
+                grad += self.l2_lambda * w
             return grad
         w0 = np.zeros(D)                                # initialize the weights to 0
         self.w = optimizer.run(gradient, x, y, w0)      # run the optimizer to get the optimal weights
